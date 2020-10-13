c7b28671d0b7f87b5d4c9088a910eb1c30ca1f5c
Miguel Ângelo <miguel.angelo@madpiece.com>
2020-10-13 09:26:21 +0300

    Add basic sctructure
    Remove original CRA code

---
+++ src/components/main.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <main>
+      {/* TODO: create individual components */}
+      <section>
+        <h1>Hero</h1>
+      </section>
+      <section>
+        <h1>Description</h1>
+      </section>
+      <section>
+        <h1>Companies</h1>
+      </section>
+      <section>
+        <h1>Partners/Sponsors</h1>
+      </section>
+    </main>
+  );
+};
+
+export default Main;
